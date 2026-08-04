564904bef357cf3561306e35a3504b6f3139f984
badalk210304onl9-alt <badalk210304.onl9@gmail.com>
2026-08-04 23:36:50 +0530

    Create search-icon.tsx

---
+++ components/icons/search-icon.tsx
@@ -0,0 +1,24 @@
+export default function SearchIcon({
+  size = 22,
+  strokeWidth = 2,
+}: {
+  size?: number;
+  strokeWidth?: number;
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="11" cy="11" r="7" />
+      <path d="m20 20-3.5-3.5" />
+    </svg>
+  );
+}
